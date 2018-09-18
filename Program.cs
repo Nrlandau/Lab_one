125040e6281736bd4e542f1ed87ac9ea6e292228
nrlandau <nrlandau@gmail.com>
2018-09-18 16:16:28 -0400

    First Commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Project_Lab_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            System.Console.WriteLine("test");
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
